55aed757547ddc390bc469fbcf409907bff81a80
sandesh-naik <sandesh@indianrails.in>
2020-11-25 22:51:02 +0530

    Initial Commit

---
+++ WeatherMap/WeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMapApp.swift
+//  WeatherMap
+//
+//  Created by Sandesh on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
